2c74c76f8d21d8495e29f90030039b06239bb839
Mondal <subrata.b.mondal@capgemini.com>
2024-02-12 01:02:25 +0530

    created user entitites

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -1,14 +1,21 @@
 package com.smart;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartContactManagerApplication {
+	
+	private static final Logger logInfo=LoggerFactory.getLogger(SmartContactManagerApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
-		System.out.println("Hi");
+		
+		logInfo.info("Main class");
+		logInfo.debug("Debug main class");
+		
 	}
 
 }
