06a26b58aaa95bb41cb4651a4e7cc5aa7a0b54e5
Mondal <subrata.b.mondal@capgemini.com>
2024-02-12 13:04:08 +0530

    implement base html page

--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -3,13 +3,17 @@ package com.smart.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.User;
 
-@RestController
+
+
+@Controller
 public class HomeController {
 
     private static final Logger log = LoggerFactory.getLogger(HomeController.class);
@@ -17,20 +21,28 @@ public class HomeController {
     @Autowired
     private UserRepository repository;
 
-    @GetMapping("/test")
-    public String test() {
-        User user = new User();
-        user.setName("Subrata ");
-        user.setEmail("subrataqa@gmail.com");
-        user.setAbout("Hello bbu");
-        
-        repository.save(user);
+    @GetMapping("/home")
+    public String homeController(Model model)
+    {
+       model.addAttribute("title","Home - Smart Contact Manager");
         
         // Logging messages
-        log.info("Test controller working perfectly...");
-        log.info("New User Added: {}", user);
-        log.debug("User details: {}", user);
+          log.info("Home controller working perfectly...");
+//        log.info("New User Added: {}", user);
+//        log.debug("User details: {}", user);
         
-        return "Working perfectly";
+        return "home";
+    }
+    @GetMapping("/about")
+    public String aboutController(Model model)
+    {
+       model.addAttribute("title","About - Smart Contact Manager");
+        
+        // Logging messages
+          log.info("Home controller working perfectly...");
+//        log.info("New User Added: {}", user);
+//        log.debug("User details: {}", user);
+        
+        return "about";
     }
 }
