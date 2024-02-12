46f8fb3fc4fd9991dc3f78eac32e0231cf100aa7
Mondal <subrata.b.mondal@capgemini.com>
2024-02-12 05:57:21 +0530

    implemented log file

--- src/main/java/com/smart/SmartContactManagerApplication.java
+++ src/main/java/com/smart/SmartContactManagerApplication.java
@@ -1,21 +1,18 @@
 package com.smart;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SmartContactManagerApplication {
 	
-	private static final Logger logInfo=LoggerFactory.getLogger(SmartContactManagerApplication.class);
+	
 
 	public static void main(String[] args) {
 		SpringApplication.run(SmartContactManagerApplication.class, args);
 		
-		logInfo.info("Main class");
 		
-		logInfo.debug("Debug main class");
 		
 	}
 
--- src/main/java/com/smart/controller/HomeController.java
+++ src/main/java/com/smart/controller/HomeController.java
@@ -1,31 +1,36 @@
 package com.smart.controller;
 
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import com.smart.dao.UserRepository;
 import com.smart.entities.User;
 
 @RestController
-public class HomeController 
-{
+public class HomeController {
 
-	@Autowired
-	private UserRepository repository;
-	@GetMapping("/test")
-	public String test()
-	{
-		User user=new User();
-		user.setName("Subrata");
-		user.setEmail("subrata@gmail.com");
-		user.setAbout("Hello bbu");
-		
-		repository.save(user);
-		System.out.println(user);
-		
-		return "Working perfetly";
-	}
+    private static final Logger log = LoggerFactory.getLogger(HomeController.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    @GetMapping("/test")
+    public String test() {
+        User user = new User();
+        user.setName("Subrata ");
+        user.setEmail("subrataqa@gmail.com");
+        user.setAbout("Hello bbu");
+        
+        repository.save(user);
+        
+        // Logging messages
+        log.info("Test controller working perfectly...");
+        log.info("New User Added: {}", user);
+        log.debug("User details: {}", user);
+        
+        return "Working perfectly";
+    }
 }
